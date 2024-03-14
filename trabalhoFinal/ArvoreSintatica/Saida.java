4c342013feb428526b99c4998b9e13e87cf72003
Bruno Chim Silveira <bcsilveira@inf.ufpel.edu.br>
2024-03-13 22:47:29 -0300

    gitignore Saida.java

--- trabalhoFinal/ArvoreSintatica/Saida.java
+++ trabalhoFinal/ArvoreSintatica/Saida.java
@@ -1,16 +1,12 @@
 public static void main(String[] args) {
-float a;
-float b;
-a = 20.0;
-b = 10.0;
-System.out.println(a);
-System.out.println(b);
-if(a > 2.0){
-System.out.println(1.0);
-}
-while(a > b){
-System.out.println(a);
-a = x - 1.0;
+float i;
+float t;
+t = 1.0;
+i = 11.0;
+System.out.println(t);
+while(i > t){
+System.out.println(i);
+i = i - 1.0;
 }
 return 0.0;
 }
